f6328f547dfedfb43125ecd389df1f58395396bb
Jared <jared@Zipcoders-MacBook-Pro-3.local>
2024-07-05 14:26:17 -0400

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,47 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
 public class Main {
 
+    public static void playGame(){
+        int usersGuess;
+        int guessCount;
+        int computersNum = (int)(100 * (Math.random()) + 1);
+
+        guessCount = 0;
+        System.out.println("What is your first guess ");
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            usersGuess = scanner.nextInt();
+            guessCount++;
+            if(usersGuess == computersNum){
+                System.out.println("You win!");
+                break;
+            }
+            if(guessCount==5){
+                System.out.println("You lose");
+                continue;
+            }
+                if(usersGuess<computersNum){
+                    System.out.println("Your guess is too low");
+                }else if(usersGuess>computersNum){
+                    System.out.println("Your guess is too high");
+                }
+        }
+    }
+
     public static void main(String[] args){
-        
+        System.out.println("Pick a number between 1 and 100\n");
+        Scanner scan = new Scanner(System.in);
+        String playAgain = "y";
+        do {
+            playGame();
+            System.out.println("Would you like to play again? y/n");
+            playAgain = scan.nextLine().toLowerCase();
+        } while (playAgain.equals("y"));
+
+
     }
 }
